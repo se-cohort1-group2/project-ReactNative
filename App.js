768a009815d9b3d988af035dc82afc4d3cc4a114
phoebeykq <113828692+phoebeykq@users.noreply.github.com>
2023-05-22 21:06:14 +0800

    added BottomTabNavigator

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+function MapScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <Text>Map Screen</Text>
+        </View>
+    )
+}
+
+function TaxiScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <Text>Taxi Screen</Text>
+        </View>
+    )
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto"/>
-        </View>
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+                        if (route.name === "Map") {
+                            iconName = focused ? "ios-map" : "ios-map-outline";
+                        } else if (route.name === "Taxi") {
+                            iconName = focused ? "ios-car" : "ios-car-outline";
+                        }
+                        return (<Ionicons name={iconName} size={size} color={color} />)
+                    },
+                    tabBarActiveTintColor: "#007AFF",
+                    tabBarInactiveTintColor: "gray",
+                })}
+            >
+                <Tab.Screen name="Map" component={MapScreen}/>
+                <Tab.Screen name="Taxi" component={TaxiScreen}/>
+            </Tab.Navigator>
+        </NavigationContainer>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
+        backgroundColor: "#fff",
     },
 })
